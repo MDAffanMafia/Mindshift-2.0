31cc631b94fcb2c61affcddaae24d948a403ea5f
MDAffanMafia <shaikhaffan005@gmail.com>
2023-11-03 21:59:24 +0530

    Added feature/fileUpload

--- backend/backendMindshift/main/models.py
+++ backend/backendMindshift/main/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    content=models.ImageField(upload_to="prodImages",default="shaikh3@gmail.com")
--- backend/backendMindshift/main/views.py
+++ backend/backendMindshift/main/views.py
@@ -2,24 +2,38 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from mlModel import word_recog
 from mlModel import bipolar_model
+from main.models import Post
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 def userBipolar(request):
     if request.method=='POST':
-        file=request.FILES.get('file')       
-        
-    dataFile=["i am happy","Juuuuuuuuuuuuuuuuussssst Chillin!!","thanks to all the haters up in my face all day! ","life is wonderfull","be cheerfull","i am miserable","just laughing","kicking"]
-    happyNum=0
-    sadNum=0
-    for d in dataFile: 
-       if word_recog.analyze_sentiment(d)==True:
-           print("this was views",d)
-           happyNum+=1
-       else:
-           sadNum+=1;    
-    if bipolar_model.isBipolar(happyNum,sadNum):
-        print("the person is suffering from bipolar")
-    else:
-        print("the person is ok")    
-    return render(request,'userBipolar.html')
+        file=request.FILES.get('file')  
+        newFile=Post(content=file)
+        newFile.save()     
+        print("this")
+        f = open("D:\\mindshift\\backend\\backendMindshift\\media\\prodImages\\userData.txt", "r")
+        print(f.read())
+        wordData=[]
+        datafile=f.readlines()
+        happyNum=0
+        sadNum=0
+        status=""
+        for line in datafile:
+            for word in line.split():
+                wordData.insert(word)
+        for d in wordData: 
+           if word_recog.analyze_sentiment(d)==True:
+               print("this was views",d)
+               happyNum+=1
+           else:
+               sadNum+=1;    
+        if bipolar_model.isBipolar(happyNum,sadNum):
+           status="the person is suffering from bipolar"
+           print("the person is suffering from bipolar")
+        else:
+           status="you are ok"
+           print("the person is ok")    
+        return render(request,'userBipolar.html',{'curStatus':status}) 
+    nostate=""  
+    return render(request,'userBipolar.html',{'curStatus':nostate})
